b70c14eab801129204f152ffbc27cd40ee6281d6
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-24 01:48:36 +0300

    feat(app): Setup radix UI

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div>
+        <h1>Products page</h1>
+        <Button>New Product</Button>
+    </div>
   )
 }
 
