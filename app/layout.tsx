b2bf65fd24733fff6506268f8273fff04a20a191
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-24 11:33:37 +0300

    feat(app): handle form submission

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-      <Theme appearance="light" accentColor="grass">
+      <Theme appearance="light">
           <NavBar />
           <main className='p-5'>
             {children}
--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -1,23 +1,41 @@
 "use client"
 
+import axios from 'axios'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { useForm } from 'react-hook-form'
 import React from 'react'
+import { useRouter } from 'next/navigation'
+
+interface ProductionForm {
+    name: string
+    price: string
+    category: string
+    description: string
+}
 
 const NewIssuePage = () => {
+    const router =  useRouter()
+
+    const { register, handleSubmit } = useForm<ProductionForm>()
+
   return (
-    <div className='space-y-3'>
+    <form className='space-y-3' onSubmit={handleSubmit(async(data: ProductionForm)=> {
+        data.price = parseFloat(data.price)
+        const product = await axios.post('/api/products', data)
+        router.push('/products')
+    })}>
         <TextField.Root>
-            <TextField.Input  placeholder='Name'/>
+            <TextField.Input  placeholder='Name' {...register('name')}/>
         </TextField.Root>
         <TextField.Root>
-            <TextField.Input  placeholder='Price'/>
+            <TextField.Input  placeholder='Price' {...register('price')}/>
         </TextField.Root>
         <TextField.Root>
-            <TextField.Input  placeholder='Category'/>
+            <TextField.Input  placeholder='Category' {...register('category')}/>
         </TextField.Root>
-        <TextArea placeholder="Description" />
+        <TextArea placeholder="Description" {...register('description')}/>
         <Button>Submit New Product</Button>
-    </div>
+    </form>
   )
 }
 
