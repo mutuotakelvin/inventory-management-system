5a8e74478ba7dc6f1fe3933bbb525ff849d698d0
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-24 11:56:41 +0300

    feat(app): Handle errors

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createProductSchema = z.object({
-    name: z.string(),
-    price: z.number(),
-    description: z.string(),
-    category: z.string()
+    name: z.string().min(1, "Name is required").max(255, "Name is too long"),
+    price: z.number().min(0, "Price must be a positive number"),
+    description: z.string().min(1, "Description is required").max(255, "Description is too long"),
+    category: z.string().min(1, "Category is required").max(255, "Category is too long")
 })
 
 export async function POST( request: NextRequest){
@@ -14,7 +14,7 @@ export async function POST( request: NextRequest){
     const validation = createProductSchema.safeParse(body)
     
     if(!validation.success){
-        return NextResponse.json(validation.error.errors, {status: 400})
+        return NextResponse.json(validation.error.format(), {status: 400})
     }
 
     const newProduct = await prisma.product.create({
--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import axios from 'axios'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
 import { useForm } from 'react-hook-form'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { set } from 'zod'
 
 interface ProductionForm {
     name: string
@@ -15,27 +16,40 @@ interface ProductionForm {
 
 const NewIssuePage = () => {
     const router =  useRouter()
+    const [error, setError] = useState('')
 
     const { register, handleSubmit } = useForm<ProductionForm>()
 
   return (
-    <form className='space-y-3' onSubmit={handleSubmit(async(data: ProductionForm)=> {
-        data.price = parseFloat(data.price)
-        const product = await axios.post('/api/products', data)
-        router.push('/products')
-    })}>
-        <TextField.Root>
-            <TextField.Input  placeholder='Name' {...register('name')}/>
-        </TextField.Root>
-        <TextField.Root>
-            <TextField.Input  placeholder='Price' {...register('price')}/>
-        </TextField.Root>
-        <TextField.Root>
-            <TextField.Input  placeholder='Category' {...register('category')}/>
-        </TextField.Root>
-        <TextArea placeholder="Description" {...register('description')}/>
-        <Button>Submit New Product</Button>
-    </form>
+    <div>
+        { error &&
+            <Callout.Root color='red' className='mb-3'>
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+        <form className='space-y-3 ' onSubmit={handleSubmit(async(data: ProductionForm)=> {
+            data.price = parseFloat(data.price)
+
+            try {
+                const product = await axios.post('/api/products', data)
+                router.push('/products')
+            } catch (error) {
+                setError('An unexpected error occurred.')
+            }
+        })}>
+            <TextField.Root>
+                <TextField.Input  placeholder='Name' {...register('name')}/>
+            </TextField.Root>
+            <TextField.Root>
+                <TextField.Input  placeholder='Price' {...register('price')}/>
+            </TextField.Root>
+            <TextField.Root>
+                <TextField.Input  placeholder='Category' {...register('category')}/>
+            </TextField.Root>
+            <TextArea placeholder="Description" {...register('description')}/>
+            <Button>Submit New Product</Button>
+        </form>
+
+    </div>
   )
 }
 
