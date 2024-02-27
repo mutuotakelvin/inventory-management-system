d2725a05e29fcbcc83f662e66754e3a92563aca1
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-27 13:34:10 +0300

    feat(app): update the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 import { MdOutlineInventory } from "react-icons/md";
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react'
+import { Container } from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath = usePathname()
@@ -16,32 +17,38 @@ const NavBar = () => {
         {href: '/products/list', label: 'Products'}
     ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'> <MdOutlineInventory className='h-6 w-6'/></Link>
-        <ul className='flex space-x-6'>
-            {
-                links.map(({href, label}) => (
-                    <li key={`${href}${label}`} >
-                        <Link 
-                        href={href}
-                        className={classNames({
-                            'text-zinc-900': href === currentPath,
-                            'text-zinc-500': href !== currentPath,
-                            'hover:text-zinc-800 transition-colors': true
-                        })}
-                        >{ label}</Link>
-                    </li>
-                ))
-            }
-        </ul>
-        <div>
-            {
-                status === 'authenticated' && (<Link href="/api/auth/signout">Sign Out</Link>)
-            }
-            {
-                status === 'unauthenticated' && (<Link href="/api/auth/signin">Sign In</Link>)
-            }
-        </div>
+    <nav className='border-b mb-5 px-5 py-3'>
+        <Container>
+            <div className='flex justify-between items-center '>
+                <div className='ml-3 flex gap-4 items-center'>
+                    <Link href='/'> <MdOutlineInventory className='h-6 w-6'/></Link>
+                    <ul className='flex space-x-6'>
+                        {
+                            links.map(({href, label}) => (
+                                <li key={`${href}${label}`} >
+                                    <Link 
+                                    href={href}
+                                    className={classNames({
+                                        'text-zinc-900': href === currentPath,
+                                        'text-zinc-500': href !== currentPath,
+                                        'hover:text-zinc-800 transition-colors': true
+                                    })}
+                                    >{ label}</Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    {
+                        status === 'authenticated' && (<Link href="/api/auth/signout">Sign Out</Link>)
+                    }
+                    {
+                        status === 'unauthenticated' && (<Link href="/api/auth/signin">Sign In</Link>)
+                    }
+                </div>
+            </div>
+        </Container>
     </nav>
   )
 }
