86e04b1c8b7e4d3c8fe67f1efa88dee70f2b12ce
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-27 11:42:10 +0300

    feat(app): handle errors

--- app/products/[id]/DeleteProductButton.tsx
+++ app/products/[id]/DeleteProductButton.tsx
@@ -1,36 +1,54 @@
 "use client"
 import { AlertDialog, Button } from '@radix-ui/themes'
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 
 const DeleteProductButton = ({productId}: { productId: number}) => {
+    const [error, setError] = useState(false)
     const router = useRouter()
-  return (
-    <AlertDialog.Root>
-        <AlertDialog.Trigger>
-            <Button color='red'>Delete Product</Button>
-        </AlertDialog.Trigger>
-        <AlertDialog.Content>
-            <AlertDialog.Title>Delete Product</AlertDialog.Title>
-            <AlertDialog.Description>
-                Are you sure you want to delete this product?
-            </AlertDialog.Description>
-            <div className='mt-4 flex gap-4 items-center'>
-                <AlertDialog.Cancel>
-                    <Button  variant='soft' color='gray'>Cancel</Button>
-                </AlertDialog.Cancel>
-                <AlertDialog.Action>
-                    <Button color='red' onClick={ async() => {
-                       await axios.delete('/api/products/' + productId)
-                       router.push('/products')
-                       router.refresh()
-                    }}>Delete</Button>
-                </AlertDialog.Action>
-            </div>
-        </AlertDialog.Content>
-    </AlertDialog.Root>
-  )
+
+    const deleteProduct = async () => {
+        try {
+            await axios.delete('/api/products/' + productId)
+            router.push('/products')
+            router.refresh()
+        } catch (error) {
+            setError(true)
+        }
+    }
+    return (
+        <>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color='red'>Delete Product</Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Delete Product</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        Are you sure you want to delete this product?
+                    </AlertDialog.Description>
+                    <div className='mt-4 flex gap-4 items-center'>
+                        <AlertDialog.Cancel>
+                            <Button  variant='soft' color='gray'>Cancel</Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                        <Button color='red' onClick={ deleteProduct}>Delete</Button>
+                        </AlertDialog.Action>
+                    </div>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Error</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        There was an error deleting the product.
+                    </AlertDialog.Description>
+                    <Button className='mt-2' color='gray' variant='soft' onClick={() => setError(false)}>OK</Button>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+        </>
+)
 }
 
 export default DeleteProductButton
