7e375816648b463d59a71fc9175eafbf81b97741
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-28 05:50:19 +0300

    feat(app): build filter component

--- app/products/list/ProductActions.tsx
+++ app/products/list/ProductActions.tsx
@@ -1,15 +1,17 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+import ProductStatusFilter from "./ProductStatusFilter";
 
 const ProductActions = () => {
   return (
-    <div className='mb-3 mt-3'>
-          <Button>
-              <Link href='/products/new'>New Product</Link>
-          </Button>
-        </div>
-  )
-}
+    <div className="mb-3 mt-3 flex justify-between">
+      <ProductStatusFilter />
+      <Button>
+        <Link href="/products/new">New Product</Link>
+      </Button>
+    </div>
+  );
+};
 
-export default ProductActions
+export default ProductActions;
---
+++ app/products/list/ProductStatusFilter.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { Status } from '@prisma/client'
+import { Select } from '@radix-ui/themes'
+import React from 'react'
+
+const ProductStatusFilter = () => {
+    const statuses: {label: string, value?: Status}[] = [
+        { label: 'All'},
+        { label: 'In Stock', value: 'IN_STOCK'},
+        { label: 'Out of Stock', value: 'OUT_OF_STOCK'},
+    ]
+  return (
+    <Select.Root>
+        <Select.Trigger placeholder='Filter by status...'>
+            <Select.Content>
+                {
+                    statuses.map((status, index) => (
+                        <Select.Item key={index} value={status.value || ''}>{status.label}</Select.Item>
+                    ))
+                }
+            </Select.Content>
+        </Select.Trigger>
+    </Select.Root>
+  )
+}
+
+export default ProductStatusFilter
