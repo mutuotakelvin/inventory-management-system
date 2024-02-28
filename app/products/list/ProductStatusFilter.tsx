af2356b07cd0f3620d6bf93bd32570ad310a2cc6
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-02-28 12:22:48 +0300

    feat(app): fix filter functionality

--- app/products/list/ProductStatusFilter.tsx
+++ app/products/list/ProductStatusFilter.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const ProductStatusFilter = () => {
     const router = useRouter()
+    const searchParams = useSearchParams()
+
     const statuses: {label: string, value?: Status}[] = [
         { label: 'All' }, 
         { label: 'In Stock', value: 'IN_STOCK'},
@@ -12,8 +14,16 @@ const ProductStatusFilter = () => {
     ];
     
     return (
-        <Select.Root onValueChange={(status) => {
-            const query = status !== ' ' ? `?status=${status}` : ''
+        <Select.Root
+        defaultValue={searchParams.get('status') || ' '}
+         onValueChange={(status) => {
+            const params = new URLSearchParams()
+
+            if(status) params.append('status',status)
+            if (searchParams.get('orderBy'))
+                params.append('orderBy', searchParams.get('orderBy')!)
+            
+            const query = params.size ? '?' + params.toString() : ' '
             router.push('/products/list' + query)
         }}>
             <Select.Trigger placeholder='Filter by status...' />
